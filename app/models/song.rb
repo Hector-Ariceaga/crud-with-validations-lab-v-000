cbf77d7bccf63ec9d5887892cdbd77924b102a18
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-07 17:20:37 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,6 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: {scope: :release_year}
   validates :released, inclusion: {in: [true, false]}
-  validates :release_year, presence: true, if: -> {:released == true}
-  validates :release_year, numericality: {less_than_or_equal_to: Time.current.year}
+  validates :release_year, numericality: {less_than_or_equal_to: Time.current.year}, if: -> {:released == true}
   validates :artist_name, presence: true
 end
