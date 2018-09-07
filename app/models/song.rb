9af7c0a8d76747840158ec58e93ae2b5846c5b86
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-07 17:01:46 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, uniqueness: true, if: -> {self.artist_name == song.artist_name}
+  validates :title, uniqueness: {scope: :release_year}
 end
